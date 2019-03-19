d32753380de60a35eece3843ee9be08fe9428ab1
chaimaby <chaima.benyounes1@gmail.com>
2019-03-19 11:17:54 +0100

    [add] listgroup in html

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Contact } from '../models/contact.model';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
+  @Input() contact : Contact;
+  
   constructor() { }
 
   ngOnInit() {
